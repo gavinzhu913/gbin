9cf2582aca68507fab60a189699a8fa080978682
gavin <gavin@xmail.vip>
2022-08-25 11:45:51 +0800

    v2.0.0

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/gavinzhudev/gbin
+module github.com/gavinzhudev/gbin/v2
 
 go 1.18
--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 package main
 
 func main(){
-    println("hello gbin v1.0.1")
+    println("hello gbin v2.0.0")
 }
